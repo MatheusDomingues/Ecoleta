92681a1dc76fcea04c1bb37e8284e9d875fa1c9d
Matheus Domingues <62768805+MatheusDomingues@users.noreply.github.com>
2020-06-07 14:07:14 -0300

    Add files via upload

---
+++ public/scripts/intro.js
@@ -0,0 +1,64 @@
+// variaveis, tipos de dados
+// let myvar = "He"
+// const myconst = "He"
+
+// document.write(myvar + myconst)
+
+// string
+// const string1 = "Isso é uma String"
+// const string2 = 'Isso também é uma String'
+// const string3 = `Isso é uma String também`
+
+
+// Number 
+// const n1 = 1
+// const n2 = 12
+// document.write(n1 + n2)
+
+// Boolean
+// const bTrue = true
+// const bFalse = false
+// document.write(bFalse)
+
+// Objetos possuem propriedades e funcionalidades
+// const pessoa = {
+//  altura: "1,80m",
+//  idade: 24,
+//  solteiro: true,
+//  correr(){
+//    document.write("executar uma grande logica de correr!")
+//   }
+// }
+
+// pessoa.correr()
+
+// Array - Vetores
+// const colecaoDeBolinhas = ["blue", "green", 1, {name: "My Name"}]
+
+// document.write(colecaoDeBolinhas[3].name)
+// Funções - Funcionalidades - Atalhos - Reuso de código
+
+// registrar função
+// function sayMyName(name) {
+  
+//   document.write(name)
+// }
+
+// executar
+// sayMyName("Douglas")
+// sayMyName("Valeska")
+// sayMyName("Kyam")
+
+// condicionais
+// const notaFinal = 5
+
+// if( notaFinal < 5 ) {
+//   document.write("Reprovado!")
+// } else {
+//   document.write("Aprovado!")
+// }
+
+// Loop repetições
+// for (i = 0; i < 10; i++) {
+//   document.write(`<p>${i}</p>`)
+// }
---
+++ src/server.js
@@ -0,0 +1,94 @@
+const express = require('express');
+const server = express();
+
+// pegar o banco de dados
+const db = require('./database/db.js');
+
+// configurar pasta publica
+server.use(express.static("public"));
+
+// Habilitar o uso do req.body na nossa aplicação
+server.use(express.urlencoded({ extended: true }));
+
+// Usando tamplate engine 
+const nunjucks = require('nunjucks');
+nunjucks.configure("src/views", {
+  express: server,
+  noCache: true
+});
+
+// configurar caminhos da minha aplicação
+// pagina inicial
+// req: Requisição
+// res: Resposta
+server.get("/", (req, res) => {
+  return res.render("index.html");
+});
+
+server.get("/create-point", (req, res) => {
+
+  // req.query: Query Strings da nossa URL
+  // console.log(req.query);
+
+  return res.render("create-point.html", { saved: true });
+});
+
+server.post("/savepoint", (req, res) => {
+
+  // req.body: O corpo do nosso formulário
+  // console.log(req.body);
+
+  // inserir dados no banco de dados
+  const query = `
+    INSERT INTO places (
+      image,
+      name,
+      adress,
+      adress2,
+      state,
+      city,
+      items
+    ) VALUES (?, ?, ?, ?, ?, ?, ?);
+  `;
+
+  const values = [
+    req.body.image,
+    req.body.name,
+    req.body.address,
+    req.body.address2,
+    req.body.state,
+    req.body.city,
+    req.body.items
+  ];
+
+  function afterInsertData(err) {
+    if(err) {
+      return console.log(err);
+    };
+
+    console.log("Cadastrado com Sucesso!");
+    console.log(this);
+
+    return res.render("create-point.html", { saved: true });
+  };
+
+  db.run(query, values, afterInsertData);
+});
+
+server.get("/search", (req, res) => {
+
+  // pegar os dados do banco de dados
+  db.all(`SELECT * FROM places`, function(err, rows) {
+    if(err) {
+      return console.log(err);
+    };
+
+    const total = rows.length
+
+    // mostrar a pagina html com os dados do banco de dados
+    return res.render("search-results.html", { places: rows, total });
+  });
+});
+
+// ligar o servidor
+server.listen(3000);
