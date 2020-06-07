bbe48a5f1d23153fe9a945c154e02fde4198daed
Matheus Domingues <62768805+MatheusDomingues@users.noreply.github.com>
2020-06-07 14:52:28 -0300

    Add files via upload
    back-end ok

--- src/server.js
+++ src/server.js
@@ -30,7 +30,7 @@ server.get("/create-point", (req, res) => {
   // req.query: Query Strings da nossa URL
   // console.log(req.query);
 
-  return res.render("create-point.html", { saved: true });
+  return res.render("create-point.html");
 });
 
 server.post("/savepoint", (req, res) => {
@@ -63,7 +63,8 @@ server.post("/savepoint", (req, res) => {
 
   function afterInsertData(err) {
     if(err) {
-      return console.log(err);
+      console.log(err);
+      return res.send("Erro no cadastro!");
     };
 
     console.log("Cadastrado com Sucesso!");
@@ -77,8 +78,16 @@ server.post("/savepoint", (req, res) => {
 
 server.get("/search", (req, res) => {
 
+  const search = req.query.search 
+
+  if(search == "") {
+    // pesquisa vazia
+    return res.render("search-results.html", {total: 0});
+  }
+
+
   // pegar os dados do banco de dados
-  db.all(`SELECT * FROM places`, function(err, rows) {
+  db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function(err, rows) {
     if(err) {
       return console.log(err);
     };
